3f66bb913aa41ae2b2410c4abfe6dec23fbbbef6
Andy Sherwood <xinmyname@gmail.com>
2016-07-12 22:45:17 -0700

    Initial Commit

---
+++ AllTheLoot/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AllTheLoot
+//
+//  Created by Andy Sherwood on 7/12/16.
+//  Copyright © 2016 Andy Sherwood. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
